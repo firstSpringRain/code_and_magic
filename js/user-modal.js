af37436df968daed2b1ead971c834acb667897aa
Ляйсан Ахметгаряева <120134977+firstSpringRain@users.noreply.github.com>
2025-05-09 01:42:31 +0300

    Add files via upload

---
+++ js/user-modal.js
@@ -0,0 +1,44 @@
+import { isEscapeKey, isEnterKey } from './util.js';
+
+const userModalElement = document.querySelector('.setup');
+const userModalOpenElement = document.querySelector('.setup-open');
+const userModalCloseElement = userModalElement.querySelector('.setup-close');
+
+const onPopupEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUserModal();
+  }
+};
+
+function openUserModal () {
+  userModalElement.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscKeydown);
+};
+
+function closeUserModal () {
+  userModalElement.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscKeydown);
+};
+
+userModalOpenElement.addEventListener('click', () => {
+  openUserModal();
+});
+
+userModalOpenElement.addEventListener('keydown', (evt) => {
+  if(isEnterKey(evt)) {
+    openUserModal();
+  }
+});
+
+userModalCloseElement.addEventListener('click', () => {
+  closeUserModal();
+});
+
+userModalCloseElement.addEventListener('keydown', (evt) => {
+  if (isEnterKey(evt)) {
+    closeUserModal();
+  }
+});
+
+export {openUserModal, closeUserModal};
